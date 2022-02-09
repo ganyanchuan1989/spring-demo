a2231d59a6d0ad62878b5e7f4817ed11f649f4c8
ganxunzou <ganxunzou@163.com>
2022-02-09 23:22:07 +0800

    增加事务处理

--- mybatis-demo/src/main/java/com/gxz/demo/mapper/UserMapperImpl.java
+++ mybatis-demo/src/main/java/com/gxz/demo/mapper/UserMapperImpl.java
@@ -15,6 +15,24 @@ public class UserMapperImpl implements UserMapper {
 
     @Override
     public List<User> selectUsers() {
+        User user = new User();
+        user.setName("gxz");
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.addUser(user);
+
+        mapper.deleteUser(1);
+
         return sqlSession.getMapper(UserMapper.class).selectUsers();
+    }
+
+    @Override
+    public int addUser(User user) {
+        return sqlSession.getMapper(UserMapper.class).addUser(user);
+    }
+
+    @Override
+    public int deleteUser(int id) {
+        return sqlSession.getMapper(UserMapper.class).deleteUser(id);
     }
 }
